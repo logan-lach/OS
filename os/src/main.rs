3f88d0bed34d328179599f94ac81024bbf788045
loganlach <lwlach@gmail.umd.edu>
2022-05-24 19:03:36 -0400

    5/24 Read Introduction and started computing enviornment

---
+++ os/src/main.rs
@@ -0,0 +1,29 @@
+
+use std::thread;
+use std::time::Duration;
+
+/* 
+
+Threading practice (Needed for OS)
+
+What this does is simulate scheduling within rust. 
+ */
+
+ 
+fn runner() {
+    for i in 1..10 {
+        println!("{}", i);
+    }
+}
+fn main() {
+
+    let mut children = vec![];
+    for _ in 1..10 {
+        children.push(thread::spawn(|| { 
+            runner() }));
+    }
+
+    for child in children {
+        child.join();
+    }
+}
